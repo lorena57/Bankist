bae4cda715b87fa76b5c1f76c35652bab64751ad
Lorena C <lorena@eunknown.org>
2022-03-23 21:01:44 -0700

    create deposits

--- script.js
+++ script.js
@@ -92,6 +92,13 @@ const CreateUserNames = function (accs) {
 CreateUserNames(accounts);
 console.log(accounts);
 
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(deposits);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -101,8 +108,6 @@ const currencies = new Map([
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
