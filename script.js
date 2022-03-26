c929f3c36c12ada3aa33841d47be180fc545c485
Lorena C <lorena@eunknown.org>
2022-03-25 19:55:34 -0700

    add conversion function

--- script.js
+++ script.js
@@ -138,3 +138,12 @@ const currencies = new Map([
 /////////////////////////////////////////////////
 
 const eurToUsd = 1.1;
+
+const totalDepositsUSD = movements
+  .filter((mov) => mov > 0)
+  .map((mov, i, arr) => {
+    return mov * eurToUsd;
+  })
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(totalDepositsUSD);
