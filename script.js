aa45a61438c57b638c3a13ddf49067178335229c
Lorena C <lorena@eunknown.org>
2022-04-19 20:38:26 -0700

    clear out account

--- script.js
+++ script.js
@@ -193,6 +193,12 @@ btnClose.addEventListener('click', function (e) {
     inputCloseUsername.value === currentAccount.username &&
     Number(inputClosePin.value) === currentAccount.pin
   ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+
+    containerApp.style.opacity = 0;
   }
 });
 
