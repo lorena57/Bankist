fb9c3f2dd48be0fc2593da7f21179ce5c474bf3d
Lorena C <lorena@eunknown.org>
2022-04-20 20:02:23 -0700

    add loan criteria

--- script.js
+++ script.js
@@ -189,6 +189,15 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
   const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    currentAccount.movements.push(amount);
+
+    updateUI(currentAccount);
+  }
 });
 
 btnClose.addEventListener('click', function (e) {
